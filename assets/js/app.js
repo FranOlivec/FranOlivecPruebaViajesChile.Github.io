a1f46d4c893df78b385963026ec6d2c07840eb82
FranOlivec <francisca.olivares.vecchiola@gmail.com>
2023-04-23 22:01:26 -0400

    Proyecto Finalizado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,4 @@
 
-$(document).ready(function() {
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-    
-        if (scroll >= $('#carouselExampleInterval').height()) {
-            $('nav').removeClass('navbar-transparent');
-            $('nav').addClass('bg-light');
-        } else {
-            $('nav').removeClass('bg-light');
-            $('nav').addClass('navbar-transparent');
-        }
-    })
-})
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
